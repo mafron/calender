6096fa76994114dfe88fa621476c759b87f0c045
mafron <r.loveteni@gmail.com>
2021-07-20 19:35:58 +0900

    add changing month function

--- main.go
+++ main.go
@@ -11,11 +11,21 @@ func main() {
 	router := gin.Default()
 
 	t := time.Now()
-	days := dateControl.GetThisMonthDays(t)
 
 	router.LoadHTMLGlob("templates/*.html")
 	router.GET("/", func(ctx *gin.Context) {
+		days := dateControl.GetThisMonthDays(t)
 		ctx.HTML(200, "calender.html", days)
+	})
+
+	router.GET("/next", func(ctx *gin.Context) {
+		t = t.AddDate(0, 1, 0)
+		ctx.Redirect(302, "/")
+	})
+
+	router.GET("/back", func(ctx *gin.Context) {
+		t = t.AddDate(0, -1, 0)
+		ctx.Redirect(302, "/")
 	})
 
 	router.Run()
